1ed32cad919ec7864f08dcdfbbd2e79816b778eb
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-13 06:27:20 +0000

    Organize files and introduce loader for plugin.
    git-svn-id: https://plugins.svn.wordpress.org/bbpress/trunk@193499 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bbpress.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: bbPress
+Plugin URI: http://bbpress.org
+Description: BuddyPress will add social networking features to a new or existing WordPress MU installation.
+Author: matt, sambauers, bbpress community, john james jacoby
+Version: 1.1
+Author URI: http://bbpress.org
+*/
+
+if ( !defined( 'BB_BOOT_DIR' ) )
+	define( 'BB_BOOT_DIR', 'bb-boot' );
+
+if ( !defined( 'BB_INCLUDES_DIR' ) )
+	define( 'BB_INCLUDES_DIR', 'bb-includes' );
+
+if ( !defined( 'BB_ADMIN_DIR' ) )
+	define( 'BB_ADMIN_DIR', 'bb-admin' );
+
+if ( !defined( 'BB_PAGES_DIR' ) )
+	define( 'BB_PAGES_DIR', 'bb-pages' );
+
+?>
