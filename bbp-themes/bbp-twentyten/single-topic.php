82cc37d5824b0bcb113d9a0f6f64cb1b96b7288c
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-21 12:37:32 +0000

    Update trunk to 2.0-beta-1
    git-svn-id: https://plugins.svn.wordpress.org/bbpress/trunk@387393 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bbp-themes/bbp-twentyten/single-topic.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Single Topic
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+
+<?php get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+				<?php do_action( 'bbp_template_notices' ); ?>
+
+				<?php if ( bbp_user_can_view_forum( array( 'forum_id' => bbp_get_topic_forum_id() ) ) ) : ?>
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<div id="bbp-topic-wrapper-<?php bbp_topic_id(); ?>" class="bbp-topic-wrapper">
+							<h1 class="entry-title"><?php bbp_topic_title(); ?></h1>
+							<div class="entry-content">
+
+								<?php bbp_get_template_part( 'bbpress/nav', 'breadcrumb' ); ?>
+
+								<?php if ( post_password_required() ) : ?>
+
+									<?php bbp_get_template_part( 'bbpress/form', 'protected' ); ?>
+
+								<?php else : ?>
+
+									<?php bbp_topic_tag_list(); ?>
+
+									<?php bbp_single_topic_description(); ?>
+
+									<div id="ajax-response"></div>
+
+									<?php bbp_get_template_part( 'bbpress/single', 'topic'   ); ?>
+
+									<?php if ( bbp_get_query_name() || bbp_has_replies() ) : ?>
+
+										<?php bbp_get_template_part( 'bbpress/pagination', 'replies' ); ?>
+
+										<?php bbp_get_template_part( 'bbpress/loop',       'replies' ); ?>
+
+										<?php bbp_get_template_part( 'bbpress/pagination', 'replies' ); ?>
+
+										<?php bbp_get_template_part( 'bbpress/form',       'reply'   ); ?>
+
+									<?php endif; ?>
+
+								<?php endif; ?>
+
+							</div>
+						</div><!-- #bbp-topic-wrapper-<?php bbp_topic_id(); ?> -->
+
+					<?php endwhile; ?>
+
+				<?php elseif ( bbp_is_forum_private( bbp_get_topic_forum_id(), false ) ) : ?>
+
+					<?php bbp_get_template_part( 'bbpress/no', 'access' ); ?>
+
+				<?php endif; ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
