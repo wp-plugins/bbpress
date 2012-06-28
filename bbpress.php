a9bf66d4ef878d4f298d52796caca44fc964ba07
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-28 20:09:42 +0000

    bbPress:
    * Merge 2.1-rc3 into trunk.
    
    
    
    git-svn-id: https://plugins.svn.wordpress.org/bbpress/trunk@565105 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bbpress.php
+++ bbpress.php
@@ -5,7 +5,7 @@
  *
  * bbPress is forum software with a twist from the creators of WordPress.
  *
- * $Id: bbpress.php 4021 2012-06-27 21:28:12Z johnjamesjacoby $
+ * $Id: bbpress.php 4027 2012-06-28 20:07:18Z johnjamesjacoby $
  *
  * @package bbPress
  * @subpackage Main
@@ -17,7 +17,7 @@
  * Description: bbPress is forum software with a twist from the creators of WordPress.
  * Author:      The bbPress Community
  * Author URI:  http://bbpress.org
- * Version:     2.1-rc2
+ * Version:     2.1-rc3
  * Text Domain: bbpress
  * Domain Path: /bbp-languages/
  */
@@ -173,7 +173,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.1-rc2'; // bbPress version
+		$this->version    = '2.1-rc3'; // bbPress version
 		$this->db_version = '203';     // bbPress DB version
 
 		/** Paths *************************************************************/
