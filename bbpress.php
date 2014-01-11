f3702cb4dda364ee13c5140f6f8173101cfe185f
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-11 18:28:27 +0000

    Merge bbPress development branch into deploy branch.
    git-svn-id: https://plugins.svn.wordpress.org/bbpress/branches/2.5@836780 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bbpress.php
+++ bbpress.php
@@ -5,7 +5,7 @@
  *
  * bbPress is forum software with a twist from the creators of WordPress.
  *
- * $Id: bbpress.php 5234 2013-12-27 22:14:18Z johnjamesjacoby $
+ * $Id: bbpress.php 5249 2014-01-11 18:20:32Z johnjamesjacoby $
  *
  * @package bbPress
  * @subpackage Main
@@ -17,7 +17,7 @@
  * Description: bbPress is forum software with a twist from the creators of WordPress.
  * Author:      The bbPress Community
  * Author URI:  http://bbpress.org
- * Version:     2.5.2
+ * Version:     2.5.3
  * Text Domain: bbpress
  * Domain Path: /languages/
  */
@@ -190,7 +190,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.5.2-5234';
+		$this->version    = '2.5.3-5249';
 		$this->db_version = '250';
 
 		/** Paths *************************************************************/
