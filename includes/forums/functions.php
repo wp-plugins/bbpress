f644c1c73b988af8a2fe2f5c406acfd5117870e3
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-12 16:42:21 +0000

    bbPress: Update trunk to latest.
    git-svn-id: https://plugins.svn.wordpress.org/bbpress/trunk@874134 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/forums/functions.php
+++ includes/forums/functions.php
@@ -1749,7 +1749,7 @@ function bbp_exclude_forum_ids( $type = 'string' ) {
 					$retval = array(
 						'key'     => '_bbp_forum_id',
 						'value'   => implode( ',', $forum_ids ),
-						'type'    => 'numeric',
+						'type'    => 'NUMERIC',
 						'compare' => ( 1 < count( $forum_ids ) ) ? 'NOT IN' : '!='
 					);
 					break;
@@ -1847,10 +1847,9 @@ function bbp_pre_get_posts_normalize_forum_visibility( $posts_query = null ) {
 
 		// Add the statuses
 		$posts_query->set( 'post_status', array_unique( array_filter( $post_stati ) ) );
-	}
 
-	// Topics Or Replies
-	if ( array_intersect( array( bbp_get_topic_post_type(), bbp_get_reply_post_type() ), $post_types ) ) {
+	// Some other post type besides bbPress's Forums, Topics, or Replies
+	} elseif ( ! array_diff( $post_types, array( bbp_get_forum_post_type(), bbp_get_topic_post_type(), bbp_get_reply_post_type() ) ) ) {
 
 		// Get forums to exclude
 		$forum_ids = bbp_exclude_forum_ids( 'meta_query' );
