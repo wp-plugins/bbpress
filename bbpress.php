4a048f8af67cbb2f751a8d03651da7ae10162366
johnjamesjacoby <johnjamesjacoby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-06 17:09:04 +0000

    Update bbPress 2.5 branch to 2.5.5.
    git-svn-id: https://plugins.svn.wordpress.org/bbpress/branches/2.5@1107002 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bbpress.php
+++ bbpress.php
@@ -5,7 +5,7 @@
  *
  * bbPress is forum software with a twist from the creators of WordPress.
  *
- * $Id: bbpress.php 5381 2014-06-06 19:58:00Z johnjamesjacoby $
+ * $Id: bbpress.php 5631 2015-03-06 17:03:13Z johnjamesjacoby $
  *
  * @package bbPress
  * @subpackage Main
@@ -17,7 +17,7 @@
  * Description: bbPress is forum software with a twist from the creators of WordPress.
  * Author:      The bbPress Community
  * Author URI:  http://bbpress.org
- * Version:     2.5.4
+ * Version:     2.5.5
  * Text Domain: bbpress
  * Domain Path: /languages/
  */
@@ -190,7 +190,7 @@ final class bbPress {
 
 		/** Versions **********************************************************/
 
-		$this->version    = '2.5.4-5380';
+		$this->version    = '2.5.5-5631';
 		$this->db_version = '250';
 
 		/** Paths *************************************************************/
